7befd103ddf3394dc47ad1236d67c93ce11bb07f
thomasyu888 <thomasyu888@gmail.com>
2016-10-13 14:54:39 -0700

    Add in R script

---
+++ dreamtools/core/stats/getAUC.R
@@ -0,0 +1,17 @@
+if (!require(pROC)) {
+  install.packages("pROC")
+}
+
+library(pROC)
+## These functions assume that the gold standard data and the predictions
+## have already been matched 
+
+## computes AUC and partial AUC focusing on sensitivity
+##
+#Assume label and prediction are matched
+GetScores <- function(label, prediction, sensitivityRange = c(0.8, 1)) {
+  roc1 <- roc(label, prediction, direction = "<")
+  AUC <- auc(roc1)[1]
+  pAUCse <- auc(roc1, partial.auc = sensitivityRange, partial.auc.focus = "sensitivity", partial.auc.correct = FALSE)[1]
+  list(AUC = AUC, pAUCse = pAUCse)
+}
